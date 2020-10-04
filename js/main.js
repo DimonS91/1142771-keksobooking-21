399ed190860bb8a87764cd7e3f7d059efef0b488
DimonS91 <dsss.front.91@gmail.com>
2020-10-04 18:31:31 +0300

    Выполнил задание доверяй, но проверяй (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const mapPin = document.querySelector('#pin').content.querySelector('.map__pin');
-// const card = document.querySelector('#card').content.querySelector('.map__card');
+const card = document.querySelector('#card').content.querySelector('.map__card');
 const mapPins = document.querySelector('.map__pins');
 const mapFilters = document.querySelector('.map__filters');
 const filterElements = mapFilters.querySelectorAll('select');
@@ -9,11 +9,18 @@ const mapPinMain = document.querySelector('.map__pin--main');
 const mapElement = document.querySelector('.map');
 const housePriceField = document.querySelector('#housing-price');
 const addForm = document.querySelector('.ad-form');
+const headingFormInput = addForm.querySelector('#title');
+const typeHouseSelect = addForm.querySelector('#type');
+const priceInput = addForm.querySelector('#price');
 const formElements = addForm.querySelectorAll('fieldset');
 const addressForm = document.querySelector('#address');
 const roomNumber = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
 const capacityOptions = capacity.querySelectorAll('option');
+const timeIn = document.querySelector('#timein');
+// const timeInOption = timeIn.querySelectorAll('option');
+const timeOut = document.querySelector('#timeout');
+const timeOutOption = timeOut.querySelectorAll('option');
 housePriceField.classList.remove('hidden');
 
 const OBJECT_TITLES = ['Большая квартира', 'Шикарный петхаус', 'Гостевой дом', 'Коттедж для большой семьи', 'Номер в мотеле'];
@@ -26,13 +33,16 @@ const MAIN_PIN_WIDTH = 65;
 const MAIN_PIN_HEIGHT = 65;
 const offsetX = 25;
 const offsetY = 35;
+const HEADING_MIN_LENGTH = 30;
+const HEADING_MAX_LENGTH = 100;
 
-// const houseTypes = {
-//   'palace': 'Дворец',
-//   'flat': 'Квартира',
-//   'house': 'Дом',
-//   'bungalow': 'Бунгало'
-// };
+
+const houseTypes = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalow': 'Бунгало'
+};
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -72,7 +82,7 @@ const getPins = (count) => {
         'photos': getRandomArray(OBJECT_PHOTOS),
       },
       'location': {
-        'x': getRandomInt(0, 1200),
+        'x': getRandomInt(0, 1000),
         'y': getRandomInt(130, 630)
       }
     };
@@ -80,88 +90,122 @@ const getPins = (count) => {
   return array;
 };
 
-const createPin = (pin) => {
+const renderCard = (data) => {
+  const mapPopup = document.querySelector('.map__card');
+  if (mapPopup) {
+    mapPopup.remove();
+  }
+  mapElement.appendChild(createPopup(data));
+};
+
+
+const createPin = (pinsArray) => {
   const element = mapPin.cloneNode(true);
 
-  element.querySelector('img').src = pin.author.avatar;
-  element.querySelector('img').alt = pin.offer.title;
-  element.style.left = pin.location.x + offsetX + 'px';
-  element.style.top = pin.location.y + offsetY + 'px';
+  element.querySelector('img').src = pinsArray.author.avatar;
+  element.querySelector('img').alt = pinsArray.offer.title;
+  element.style.left = pinsArray.location.x + offsetX + 'px';
+  element.style.top = pinsArray.location.y + offsetY + 'px';
+
+  const openPopupMouse = (evt) => {
+    if (evt.which === 1) {
+      renderCard(pinsArray);
+    }
+  };
+  const openPopupEnter = (evt) => {
+    if (evt.keyCode === 13) {
+      renderCard(pinsArray);
+    }
+  };
+
+  element.addEventListener('mousedown', openPopupMouse);
+  element.addEventListener('keydown', openPopupEnter);
 
   return element;
 };
 
-// const createAvatarPopup = (element, incomingData) => {
-//   if (!incomingData) {
-//     element.style.display = 'none';
-//     return;
-//   } else {
-//     element.src = incomingData;
-//   }
-// };
+const createAvatarPopup = (element, incomingData) => {
+  if (!incomingData) {
+    element.style.display = 'none';
+    return;
+  } else {
+    element.src = incomingData;
+  }
+};
 
-// const createElementPopup = (element, incomingData) => {
-//   if (!incomingData) {
-//     element.style.display = 'none';
-//     return;
-//   } else {
-//     element.textContent = incomingData;
-//   }
-// };
+const createElementPopup = (element, incomingData) => {
+  if (!incomingData) {
+    element.style.display = 'none';
+    return;
+  } else {
+    element.textContent = incomingData;
+  }
+};
 
-// const renderFeaturesPopup = (element, incomingData) => {
-//   element.innerHTML = '';
-//   if (!incomingData.length) {
-//     element.style.display = 'none';
-//     return;
-//   }
-//   incomingData.forEach((feature) => {
-//     const li = document.createElement('li');
-//     li.classList.add('popup__feature');
-//     li.classList.add('popup__feature--' + feature);
-//     element.appendChild(li);
-//   });
-// };
+const renderFeaturesPopup = (element, incomingData) => {
+  element.innerHTML = '';
+  if (!incomingData.length) {
+    element.style.display = 'none';
+    return;
+  }
+  incomingData.forEach((feature) => {
+    const li = document.createElement('li');
+    li.classList.add('popup__feature');
+    li.classList.add('popup__feature--' + feature);
+    element.appendChild(li);
+  });
+};
 
-// function renderPhotosPopup(element, incomingData) {
-//   element.innerHTML = '';
-//   if (!incomingData.length) {
-//     element.style.display = 'none';
-//     return;
-//   }
-//   incomingData.forEach((photo) => {
-//     const img = document.createElement('img');
-//     img.classList.add('popup__photo');
-//     img.src = photo;
-//     img.width = '45';
-//     img.height = '40';
-//     img.alt = 'Фотография жилья';
-//     element.appendChild(img);
-//   });
-// }
+function renderPhotosPopup(element, incomingData) {
+  element.innerHTML = '';
+  if (!incomingData.length) {
+    element.style.display = 'none';
+    return;
+  }
+  incomingData.forEach((photo) => {
+    const img = document.createElement('img');
+    img.classList.add('popup__photo');
+    img.src = photo;
+    img.width = '45';
+    img.height = '40';
+    img.alt = 'Фотография жилья';
+    element.appendChild(img);
+  });
+}
 
-// const createPopup = (popup) => {
-//   const popupElement = card.cloneNode(true);
+const createPopup = (pinsArray) => {
+  const popupElement = card.cloneNode(true);
 
-//   renderPhotosPopup(popupElement.querySelector('.popup__photos'), popup.offer.photos);
-//   renderFeaturesPopup(popupElement.querySelector('.popup__features'), popup.offer.features);
-//   createElementPopup(popupElement.querySelector('.popup__description'), popup.offer.description);
-//   createElementPopup(popupElement.querySelector('.popup__text--capacity'), `${popup.offer.rooms} комнаты для ${popup.offer.guests} гостей`);
-//   createElementPopup(popupElement.querySelector('.popup__text--time'), `Заезд после ${popup.offer.checkin}, выезд до ${popup.offer.checkout}`);
-//   createElementPopup(popupElement.querySelector('.popup__type'), houseTypes[popup.offer.type]);
-//   createElementPopup(popupElement.querySelector('.popup__text--price'), `${popup.offer.price}₽/ночь`);
-//   createElementPopup(popupElement.querySelector('.popup__text--address'), popup.offer.address);
-//   createElementPopup(popupElement.querySelector('.popup__title'), popup.offer.title);
-//   createAvatarPopup(popupElement.querySelector('.popup__avatar'), popup.author.avatar);
+  renderPhotosPopup(popupElement.querySelector('.popup__photos'), pinsArray.offer.photos);
+  renderFeaturesPopup(popupElement.querySelector('.popup__features'), pinsArray.offer.features);
+  createElementPopup(popupElement.querySelector('.popup__description'), pinsArray.offer.description);
+  createElementPopup(popupElement.querySelector('.popup__text--capacity'), `${pinsArray.offer.rooms} комнаты для ${pinsArray.offer.guests} гостей`);
+  createElementPopup(popupElement.querySelector('.popup__text--time'), `Заезд после ${pinsArray.offer.checkin}, выезд до ${pinsArray.offer.checkout}`);
+  createElementPopup(popupElement.querySelector('.popup__type'), houseTypes[pinsArray.offer.type]);
+  createElementPopup(popupElement.querySelector('.popup__text--price'), `${pinsArray.offer.price}₽/ночь`);
+  createElementPopup(popupElement.querySelector('.popup__text--address'), pinsArray.offer.address);
+  createElementPopup(popupElement.querySelector('.popup__title'), pinsArray.offer.title);
+  createAvatarPopup(popupElement.querySelector('.popup__avatar'), pinsArray.author.avatar);
 
-//   return popupElement;
-// };
+  const popupClose = popupElement.querySelector('.popup__close');
 
-// const renderPopup = (pinsArray) => {
-//   pinsArray.forEach((pop) => {
-//     mapElement.appendChild(createPopup(pop));
-//   });
-// };
+  const hidePopupMouse = (evt) => {
+    if (evt.which === 1) {
+      popupElement.remove();
+    }
+  };
+
+  const hidePopupEsc = (evt) => {
+    if (evt.keyCode === 27) {
+      popupElement.remove();
+    }
+  };
+
+  popupClose.addEventListener('mousedown', hidePopupMouse);
+  window.addEventListener('keydown', hidePopupEsc);
+
+  return popupElement;
+};
 
 const renderPins = (pinsArray) => {
   pinsArray.forEach((pin) => {
@@ -170,7 +214,6 @@ const renderPins = (pinsArray) => {
 };
 
 const pinsArray = getPins(8);
-// renderPopup(pinsArray);
 
 const setAtivationAndInactivation = (bool, filter, form) => {
   filter.forEach((element) => {
@@ -190,6 +233,7 @@ const activationMap = () => {
   setAtivationAndInactivation(false, filterElements, formElements);
   renderPins(pinsArray);
   checkRoom(roomNumber.value);
+  checkTime(timeIn.value);
 };
 
 const createAddress = (coordinateX, coordinateY) => {
@@ -215,7 +259,6 @@ const onPinKeyDown = (evt) => {
 mapPinMain.addEventListener('mousedown', onPinMouseDown);
 mapPinMain.addEventListener('keydown', onPinKeyDown);
 
-
 const roomValues = {
   1: [1],
   2: [1, 2],
@@ -240,4 +283,63 @@ const checkRoom = (people) => {
 
 roomNumber.addEventListener('change', (evt) => {
   checkRoom(evt.target.value);
+});
+
+// Валидация заголовка
+
+headingFormInput.addEventListener('input', () => {
+  const valueLength = headingFormInput.value.length;
+  if (valueLength < HEADING_MIN_LENGTH) {
+    headingFormInput.setCustomValidity('Еще ' + (HEADING_MIN_LENGTH - valueLength) + ' симв')
+  } else if (valueLength > HEADING_MAX_LENGTH) {
+    headingFormInput.setCustomValidity('Удалите лишние ' + (valueLength - HEADING_MAX_LENGTH) + ' симв')
+  } else {
+    headingFormInput.setCustomValidity('');
+  }
+
+  headingFormInput.reportValidity();
+});
+
+// Валидация въезда и выезда
+
+const checkinAndCheckout = {
+  '12:00': ['12:00'],
+  '13:00': ['13:00'],
+  '14:00': ['14:00']
+};
+
+const checkTime = (time) => {
+  timeOutOption.forEach((element) => {
+    element.disabled = true;
+  });
+
+  checkinAndCheckout[time].forEach((times) => {
+    timeOutOption.forEach((element) => {
+      if (element.value === times) {
+        element.disabled = false;
+        element.selected = true;
+      }
+    });
+  });
+};
+
+timeIn.addEventListener('change', (evt) => {
+  checkTime(evt.target.value);
+});
+
+// Валидация цены
+
+const minPrice = {
+  bungalo: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000
+};
+
+const typeHouse = (type) => {
+  priceInput.setAttribute('min', minPrice[type]);
+  priceInput.setAttribute('placeholder', minPrice[type]);
+};
+typeHouseSelect.addEventListener('change', (evt) => {
+  typeHouse(evt.target.value);
 });
